49d51b218a6c975e0c4c691a7b10540b834c7628
Julia Mersing Ortiz <mariajulia.mersing@clearpay.com>
2022-09-05 16:51:07 +0200

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import Input from './components/Input';
-import Checkbox from './components/Checkbox';
-import Button from './components/Button';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
 
 const App = () => (
-  <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-      <Header heading="Sign in" paragraph="Dont have an account" href="/signUp" linkParagraph="Sign Up" />
-      <form className="mt-8 space-y-6" action="#" method="POST">
-        <input type="hidden" name="remember" value="true" />
-        <div className="rounded-md shadow-sm -space-y-px">
-          <Input label="Email" placeholder="Email" id="email" type="email" />
-          <Input label="Password" placeholder="Password" id="password" type="password" />
-        </div>
-        <div className="flex items-center justify-between">
-          <Checkbox />
-          <div className="text-sm">
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
-          </div>
-        </div>
-        <Button> Sign in </Button>
-      </form>
-    </div>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 
 );
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = ({
     <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">{heading}</h2>
     <p className="mt-2 text-center text-sm text-gray-600">
       {paragraph}
-      <a href={href} className="font-medium text-indigo-600 hover:text-indigo-500">
+      <a href={href} className="font-medium text-indigo-600 hover:text-indigo-500 ml-2">
         {linkParagraph}
       </a>
     </p>
